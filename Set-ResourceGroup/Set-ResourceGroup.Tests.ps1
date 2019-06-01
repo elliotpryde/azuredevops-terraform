a6fa2c7d14de577bfe77b17326f6f086293417e1
Elliot Pryde <elliotpryde@hotmail.co.uk>
2019-06-01 14:39:46 +0100

    Initial commit

---
+++ Set-ResourceGroup/Set-ResourceGroup.Tests.ps1
@@ -0,0 +1,28 @@
+. ".\Set-ResourceGroup\Set-ResourceGroup.ps1"
+
+Describe "Set-ResourceGroup" {
+
+    It "should not create a resource group if one already exists" {
+        Mock "Invoke-Azure" {
+            if ($args -and $args[0] -eq "group" -and $args[1] -eq "exists") {
+                return $true
+            }
+        }
+
+        Set-ResourceGroup -Name "resource_grp_name"
+
+        Assert-MockCalled Invoke-Azure -Times 0 -ParameterFilter {$args -and $args[0] -eq "group" -and $args[1] -eq "create"}
+    }
+
+    It "should create a resource group if it doesn't already exist" {
+        Mock "Invoke-Azure" -MockWith {
+            if ($args -and $args[0] -eq "group" -and $args[1] -eq "exists") {
+                return $false
+            }
+        }
+
+        Set-ResourceGroup -Name "resource_grp_name"
+
+        Assert-MockCalled Invoke-Azure -Times 1 -ParameterFilter {$args -and $args[0] -eq "group" -and $args[1] -eq "create"}
+    }
+}
---
+++ Set-ResourceGroup/Set-ResourceGroup.ps1
@@ -0,0 +1,17 @@
+. ".\Invoke-Azure\Invoke-Azure.ps1"
+
+function Set-ResourceGroup() {
+    Param(
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Name
+    )
+    Write-Output "Checking if resource group already exists..."
+    $resource_grp_exists = Invoke-Azure group exists --name $Name
+
+    if ($resource_grp_exists -ne $true) {
+        Write-Output "Creating resource group..."
+        Invoke-Azure group create --location uksouth --name $Name# | Out-String | ConvertFrom-Json
+    } else {
+        Write-Output "Resource group already exists"
+    }
+}
