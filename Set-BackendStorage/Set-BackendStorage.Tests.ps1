0c18e1bac90ad0da2007240e2059a59618999638
Elliot Pryde <elliotpryde@hotmail.co.uk>
2019-06-02 21:13:48 +0100

    First draft of Set-BackendStorage and Set-StorageContainer

---
+++ Set-BackendStorage/Set-BackendStorage.Tests.ps1
@@ -0,0 +1,8 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+. "$here\Set-BackendStorage.ps1"
+
+Describe "Set-BackendStorage" {
+
+    # BeforeAll {}
+
+}
---
+++ Set-StorageContainer/Set-StorageContainer.Tests.ps1
@@ -0,0 +1,48 @@
+. ".\Set-StorageContainer\Set-StorageContainer.ps1"
+
+Describe "Set-StorageContainer" {
+
+    It "should create a new storage account if one does not already exist" {
+        Mock "Invoke-Azure" {
+            if (
+                $args -and
+                $args[0] -eq "storage" -and
+                $args[1] -eq "container" -and
+                $args[2] -eq "exists"
+            ) {
+                return $false
+            }
+        }
+
+        Set-StorageContainer -Name "storage_container" -StorageAccountName "storage_acc_name"
+
+        Assert-MockCalled Invoke-Azure -Times 1 -ParameterFilter {
+            $args -and
+            $args[0] -eq "storage" -and
+            $args[1] -eq "container" -and
+            $args[2] -eq "create"
+        }
+    }
+
+    It "should not create a new storage account if one already exists" {
+        Mock "Invoke-Azure" {
+            if (
+                $args -and
+                $args[0] -eq "storage" -and
+                $args[1] -eq "container" -and
+                $args[2] -eq "exists"
+            ) {
+                return $false
+            }
+        }
+
+        Set-StorageContainer -Name "storage_container" -StorageAccountName "storage_acc_name"
+
+        Assert-MockCalled Invoke-Azure -Times 0 -ParameterFilter {
+            $args -and
+            $args[0] -eq "storage" -and
+            $args[1] -eq "container" -and
+            $args[2] -eq "create"
+        }
+    }
+}
