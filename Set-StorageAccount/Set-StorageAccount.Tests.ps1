f78104e077043cfa17c9725128a78e462a7583de
Elliot Pryde <elliotpryde@hotmail.co.uk>
2019-07-14 13:51:45 +0100

    Finished tests for Set-StorageAccount

--- Set-StorageAccount/Set-StorageAccount.Tests.ps1
+++ Set-StorageAccount/Set-StorageAccount.Tests.ps1
@@ -4,36 +4,68 @@ Describe "Set-StorageAccount" {
 
     Context "if the storage account name is not available and the reason is not AlreadyExists" {
 
-            It "should throw an exception" {
-                Mock "Invoke-Azure" {
-                    if (
-                        $args -and
-                        $args[0] -eq "storage" -and
-                        $args[1] -eq "account" -and
-                        $args[2] -eq "check-name"
-                    ) {
-                        return @"
-                        {
-                            "nameAvailable": "false",
-                            "reason": "AnotherReason"
-                        }
+        It "should throw an exception" {
+            Mock "Invoke-Azure" {
+                if (
+                    $args -and
+                    $args[0] -eq "storage" -and
+                    $args[1] -eq "account" -and
+                    $args[2] -eq "check-name"
+                ) {
+                    return @"
+                    {
+                        "nameAvailable": "false",
+                        "reason": "AnotherReason"
+                    }
 "@
-                    }
                 }
-
-
-                {
-                    $arguments = @{
-                        Name = "storage_account_name"
-                        ResourceGroupName = "resource_grp_name"
-                    }
-                    Set-StorageAccount $arguments
-                } | Should Throw "Could not create storage account because:"
             }
+
+
+            {
+                $arguments = @{
+                    Name = "storage_account_name"
+                    ResourceGroupName = "resource_grp_name"
+                }
+                Set-StorageAccount @arguments
+            } | Should Throw "Could not create storage account because:"
+        }
     }
 
     Context "if the storage account name is not available and the reason is AlreadyExists" {
 
+        Mock "Invoke-Azure" {
+            if (
+                $args -and
+                $args[0] -eq "storage" -and
+                $args[1] -eq "account" -and
+                $args[2] -eq "check-name"
+            ) {
+                return @"
+                {
+                    "nameAvailable": false,
+                    "reason": "AlreadyExists"
+                }
+"@
+            }
+        }
+
+        It "should not create a new storage account" {
+            $arguments = @{
+                Name = "storage_account_name"
+                ResourceGroupName = "resource_grp_name"
+            }
+            Set-StorageAccount @arguments
+
+            Assert-MockCalled Invoke-Azure -Times 0 -Scope It -ParameterFilter {
+                $args -and
+                $args[0] -eq "storage" -and
+                $args[1] -eq "account" -and
+                $args[2] -eq "create"
+            }
+        }
+
+        It "should not throw an exception if the reason is AlreadyExists" {
             Mock "Invoke-Azure" {
                 if (
                     $args -and
@@ -50,67 +82,46 @@ Describe "Set-StorageAccount" {
                 }
             }
 
-            It "should not create a new storage account" {
-                # Set-StorageAccount -Name "storage_account_name" -ResourceGroupName "resource_grp_name"
-
-                # Assert-MockCalled Invoke-Azure -Times 0 -Scope It -ParameterFilter {
-                #     $args -and
-                #     $args[0] -eq "storage" -and
-                #     $args[1] -eq "account"
-                #     $args[2] -eq "create"
-                # }
-            }
-
-# #             It "should not throw an exception if the reason is AlreadyExists" {
-# #                 Mock "Invoke-Azure" {
-# #                     if (
-# #                         $args -and
-# #                         $args[0] -eq "storage" -and
-# #                         $args[1] -eq "account" -and
-# #                         $args[2] -eq "check-name"
-# #                     ) {
-# #                         return @"
-# #                         {
-# #                             "nameAvailable": "false",
-# #                             "reason": "AlreadyExists"
-# #                         }
-# # "@
-# #                     }
-# #                 }
-
-# #                 {
-# #                     Set-StorageAccount -Name "storage_account_name" -ResourceGroupName "resource_grp_name"
-# #                 } | Should Not Throw
-# #             }
-#         }
+            {
+                $arguments = @{
+                    Name = "storage_account_name"
+                    ResourceGroupName = "resource_grp_name"
+                }
+                Set-StorageAccount @arguments
+            } | Should Not Throw
+        }
     }
 
-# #     Context "if the storage account name is available" {
+    Context "if the storage account name is available" {
 
-# #         It "should create a new storage account" {
-# #             Mock "Invoke-Azure" {
-# #                 if (
-# #                     $args -and
-# #                     $args[0] -eq "storage" -and
-# #                     $args[1] -eq "account" -and
-# #                     $args[2] -eq "check-name"
-# #                 ) {
-# #                     return @"
-# #                     {
-# #                         "nameAvailable": "true"
-# #                     }
-# # "@
-# #                 }
-# #             }
+        It "should create a new storage account" {
+            Mock "Invoke-Azure" {
+                if (
+                    $args -and
+                    $args[0] -eq "storage" -and
+                    $args[1] -eq "account" -and
+                    $args[2] -eq "check-name"
+                ) {
+                    return @"
+                    {
+                        "nameAvailable": "true"
+                    }
+"@
+                }
+            }
 
-# #             Set-StorageAccount -Name "storage_account_name" -ResourceGroupName "resource_grp_name"
+            $arguments = @{
+                Name = "storage_account_name"
+                ResourceGroupName = "resource_grp_name"
+            }
+            Set-StorageAccount @arguments
 
-# #             Assert-MockCalled Invoke-Azure -Times 1 -Scope It -ParameterFilter {
-# #                 $args -and
-# #                 $args[0] -eq "storage" -and
-# #                 $args[1] -eq "account"
-# #                 $args[2] -eq "create"
-# #             }
-# #         }
-# #     }
+            Assert-MockCalled Invoke-Azure -Times 1 -Scope It -ParameterFilter {
+                $args -and
+                $args[0] -eq "storage" -and
+                $args[1] -eq "account"
+                $args[2] -eq "create"
+            }
+        }
+    }
 }
