c6a3bb3dc931c0aaba25a5d2a2692d245f31ceae
Elliot Pryde <elliotpryde@hotmail.co.uk>
2019-07-14 13:55:31 +0100

    Finishes tests for Set-StorageContainer

--- Set-StorageContainer/Set-StorageContainer.Tests.ps1
+++ Set-StorageContainer/Set-StorageContainer.Tests.ps1
@@ -1,3 +1,4 @@
+. ".\Invoke-Azure\Invoke-Azure.ps1"
 . ".\Set-StorageContainer\Set-StorageContainer.ps1"
 
 Describe "Set-StorageContainer" {
@@ -16,7 +17,7 @@ Describe "Set-StorageContainer" {
 
         Set-StorageContainer -Name "storage_container" -StorageAccountName "storage_acc_name"
 
-        Assert-MockCalled Invoke-Azure -Times 1 -ParameterFilter {
+        Assert-MockCalled Invoke-Azure -Times 1 -Scope It -ParameterFilter {
             $args -and
             $args[0] -eq "storage" -and
             $args[1] -eq "container" -and
@@ -32,13 +33,13 @@ Describe "Set-StorageContainer" {
                 $args[1] -eq "container" -and
                 $args[2] -eq "exists"
             ) {
-                return $false
+                return $true
             }
         }
 
         Set-StorageContainer -Name "storage_container" -StorageAccountName "storage_acc_name"
 
-        Assert-MockCalled Invoke-Azure -Times 0 -ParameterFilter {
+        Assert-MockCalled Invoke-Azure -Times 0 -Scope It -ParameterFilter {
             $args -and
             $args[0] -eq "storage" -and
             $args[1] -eq "container" -and
