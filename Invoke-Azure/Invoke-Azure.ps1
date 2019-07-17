0d32e7982ecf7d05909e9bd37a8a08272c3f2d3d
Elliot Pryde <elliotpryde@hotmail.co.uk>
2019-07-17 20:04:16 +0100

    Invoke-Azure now correctly throwing exception when az returns a non-zero exit code

--- Invoke-Azure/Invoke-Azure.ps1
+++ Invoke-Azure/Invoke-Azure.ps1
@@ -1,3 +1,7 @@
 function Invoke-Azure {
     & az $args
+    $AzureExitCode = $LastExitCode
+    if ($AzureExitCode -ne 0) {
+        throw "The Azure CLI returned a non-zero exit code ($AzureExitCode) when running {az $args}: $AzureExitCode"
+    }
 }
